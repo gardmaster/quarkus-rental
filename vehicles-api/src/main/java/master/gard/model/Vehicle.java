e05b5bf65795817f8f552fa892e60f2d0a6d5938
Giovanni Duarte <garduarte94@gmail.com>
2025-08-02 10:30:13 -0300

    adding vehicle entity and updating pom

---
+++ vehicles-api/src/main/java/master/gard/model/Vehicle.java
@@ -0,0 +1,91 @@
+package master.gard.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import master.gard.model.enums.VehicleStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Entity
+@Table(name = "vehicles")
+public class Vehicle {
+
+    private static final Map<VehicleStatus, Set<VehicleStatus>> VEHICLE_STATE_MACHINE = new HashMap<>() {
+    };
+
+    static {
+        VEHICLE_STATE_MACHINE.put(VehicleStatus.AVAILABLE, Set.of(VehicleStatus.RENTED, VehicleStatus.UNDER_MAINTENANCE));
+        VEHICLE_STATE_MACHINE.put(VehicleStatus.RENTED, Set.of(VehicleStatus.AVAILABLE, VehicleStatus.UNDER_MAINTENANCE));
+        VEHICLE_STATE_MACHINE.put(VehicleStatus.UNDER_MAINTENANCE, Set.of(VehicleStatus.AVAILABLE));
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(name = "brand")
+    private String brand;
+
+    @Setter
+    @Column(name = "manufacture_year")
+    private Long year;
+
+    @Setter
+    @Column(name = "model")
+    private String model;
+
+    @Setter
+    @Column(name = "engine")
+    private String engine;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private VehicleStatus status;
+
+    protected Vehicle() {
+    }
+
+    public Vehicle(String brand, Long year, String model, String engine) {
+        this.brand = brand;
+        this.year = year;
+        this.model = model;
+        this.engine = engine;
+        this.status = VehicleStatus.AVAILABLE;
+    }
+
+    public boolean isAvailable() {
+        return this.status == VehicleStatus.AVAILABLE;
+    }
+
+    public boolean isRented() {
+        return this.status == VehicleStatus.RENTED;
+    }
+
+    public boolean isUnderMaintenance() {
+        return this.status == VehicleStatus.UNDER_MAINTENANCE;
+    }
+
+    public void setStatus(VehicleStatus incomingStatus) {
+
+        if (incomingStatus == null) {
+            throw new IllegalArgumentException("Incoming status cannot be null");
+        }
+
+        if (incomingStatus.equals(this.status)) {
+            return;
+        }
+
+        Set<VehicleStatus> possibleStatus = VEHICLE_STATE_MACHINE.get(this.status);
+
+        if (possibleStatus == null || !possibleStatus.contains(incomingStatus)) {
+            throw new IllegalArgumentException("Invalid status transition from " + this.status + " to " + incomingStatus);
+        }
+
+        this.status = incomingStatus;
+    }
+}
---
+++ vehicles-api/src/main/java/master/gard/model/enums/VehicleStatus.java
@@ -0,0 +1,9 @@
+package master.gard.model.enums;
+
+public enum VehicleStatus {
+
+    AVAILABLE,
+    RENTED,
+    UNDER_MAINTENANCE;
+
+}
