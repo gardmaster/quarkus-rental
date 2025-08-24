48be88823e06fbc7ac5dd42626357a4d7b49a21f
Giovanni Duarte <garduarte94@gmail.com>
2025-08-24 19:44:24 -0300

    adding new resources for booking

--- booking-api/src/main/resources/import.sql
+++ booking-api/src/main/resources/import.sql
@@ -2,5 +2,7 @@ INSERT INTO
     bookings (vehicle_id, customer_id, start_date, end_date, status)
 VALUES
     (4, '3a222578-0959-489e-b0d8-055c7e3a9448', '2024-11-20', '2024-12-02', 'FINISHED'),
-    (5, 'e4b9929e-ff24-4ad4-a23b-50ff051cf860', '2025-01-01', '2025-01-03', 'CANCELED');
+    (5, 'e4b9929e-ff24-4ad4-a23b-50ff051cf860', '2025-01-01', '2025-01-03', 'CANCELED'),
+    (6, '3a222578-0959-489e-b0d8-055c7e3a9448', '2024-12-15', '2024-12-20', 'ACTIVE'),
+    (7, 'e4b9929e-ff24-4ad4-a23b-50ff051cf860', '2024-11-25', '2024-11-30', 'CREATED');
 
